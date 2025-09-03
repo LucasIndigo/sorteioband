1aa6320261f0544f689ba553023c9f1a0044ade8
LucasIndigo <110142912+LucasIndigo@users.noreply.github.com>
2025-09-03 18:01:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const auth = new google.auth.GoogleAuth({
 const sheets = google.sheets({ version: "v4", auth });
 
 // Substitua pelo ID da sua planilha
-const SPREADSHEET_ID = "1WLV0MNzw_91W0ZQJ1QN1eOk3fD6oRw-lIRGr4S1qfWk";
-const RANGE = "Página1!B:D"; // Ajuste conforme suas colunas: A=nome, B=telefone, C=instagram
+const SPREADSHEET_ID = "1zdQzfmPRCME0fXgTlxtPoUlihYoY4aL-zVoGR96wEa8";
+const RANGE = "lpbfm!B:E"; // Ajuste conforme suas colunas: A=nome, B=telefone, C=Bairro
 
 app.get("/participantes", async (req, res) => {
   try {
